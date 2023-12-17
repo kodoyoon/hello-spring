a61eadb3012afea8a48663e870b6209de585df86
doyoon <doyoun0527@naver.com>
2023-12-17 20:23:41 +0900

    회원 웹 기능 - 등록

--- src/main/java/hello/hellospring/controller/MemberController.java
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -3,6 +3,7 @@ package hello.hellospring.controller;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MemberController {
@@ -14,5 +15,8 @@ public class MemberController {
   this.memberService = memberService;
 
 }
-
+@GetMapping("/memebrs/new")
+  public String createForm() {
+    return "members/createMemberForm";
+}
 }
